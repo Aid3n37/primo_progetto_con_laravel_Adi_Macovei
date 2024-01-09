aae910214335d92d09d48434e9ef1001e5ff0296
Adi Macovei <adimaco37@gmail.com>
2024-01-09 20:27:47 +0100

    esercizio completato

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*parte uno del 09/01/2024*/
+Route::get('/stringa', function () {
+    return 'Esempio Stringa';
+});
+
+Route::get('/intero', function(){
+    return '1' ;
+});
+
+Route::get('/array', function(){
+    return 'ci dovrebbe essere un array';
+});
+/*parte 2 traccia del 09/01/2024*/
+Route::get('/chisiamo', function () {
+    return view('chisiamo');
+});
+Route::get('/contatti', function () {
+    return view('contatti');
+});
+Route::get('/storia', function () {
+    return view('storia');
+});
